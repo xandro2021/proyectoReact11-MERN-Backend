f61ffeaf12316a7febd6c35240274371dfac34f4
xandro2021 <xandrocastro2021@gmail.com>
2023-11-14 19:32:36 -0600

    set dotenv

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import conectarDB from './config/db.js';
 
 const app = express();
 
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
 console.log('Desde index.js');
-app.listen(4000, () => {
-  console.log('Servidor corriendo en el puerto 4000');
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 })
