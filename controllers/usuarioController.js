58566cb8077bfe95c4822380d09bbea00bf42ab0
xandro2021 <xandrocastro2021@gmail.com>
2023-11-15 20:59:40 -0600

    Add a token to recover contrasenia

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,10 +1,12 @@
 import Usuario from '../models/Usuario.js';
+import generarId from '../helpers/generarId.js';
 
 const registrar = async (req, res) => {
   // Evitar registros duplicados, email unico
   const { email } = req.body;
   const existeUsuario = await Usuario.findOne({ email });
 
+  // Si el usuario ya existe, termino la ejecucion
   if (existeUsuario) {
     const error = new Error('El usuario ya esta registrado');
     return res.status(400).json({ msg: error.message });
@@ -13,6 +15,8 @@ const registrar = async (req, res) => {
   try {
     // Creo instancia usuario en la DB (Tabla)
     const usuario = new Usuario(req.body);
+    // Genero token para recuperar contraseña
+    usuario.token = generarId();
     // Almaceno el registro en la instancia (Tabla)
     const usuarioAlmacenado = await usuario.save();
 
