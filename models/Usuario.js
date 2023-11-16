b798a3a69f95eeaa5cdd334a529b24cbe9d861d2
xandro2021 <xandrocastro2021@gmail.com>
2023-11-15 19:17:43 -0600

    Hash user password before saving it in MongoDB

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 // timestamps: true es para crear dos columnas, una de creado y otra de actualizado
 const usuarioSchema = mongoose.Schema(
@@ -35,6 +36,17 @@ const usuarioSchema = mongoose.Schema(
     timestamps: true
   }
 );
+
+// hashear password antes de guardar el registro
+usuarioSchema.pre('save', async function(next) {
+  // Si se esta modificando algo que no sea el pass, pues evito que lo vuelva a hashear
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  // this va a ser referencia al usuario que se esta enviando a guardar(save)
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 const Usuario = mongoose.model('Usuario', usuarioSchema);
 
